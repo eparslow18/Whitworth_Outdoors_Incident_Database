0b3f014e0a91bc63a6f89b35f7e3538c5137b8df
kbrown17 <kbrown17@LAB-SCI-304-09>
2016-12-06 13:06:11 -0800

    stuck.....

---
+++ FINAL _PROJECT/IncidentTrackingWebsite/WebApplication1/JavaScript/EditIncident.js
@@ -0,0 +1,9 @@
+﻿function LoadTable()
+{
+    $.post("http://cs1/WOIncidentTracking/PHP_Files/Load_Edit_Table.php",
+              function ()
+              {
+                  alert("successfully sent incident to php");
+                  window.location = "http://localhost:45082/Main.html"
+              });
+}
