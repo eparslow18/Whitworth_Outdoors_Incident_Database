0f0c19195eb39c89928e709f409b336a9d1c59c4
eparslow18 <eparslow18@LAB-SCI-214-19>
2016-12-06 22:42:56 -0800

    more formatting changes

---
+++ FINAL _PROJECT/IncidentTrackingWebsite/WebApplication1/JavaScript/FormatJava.js
@@ -0,0 +1,7 @@
+﻿$(TripStartDate).ready(function() {
+    $("#TripStartDate").keyup(function () {
+        if ($(this).val().length == 2 || $(this).val().length == 5){
+            $(this).val($(this).val() + "/");
+        }
+    });
+});
